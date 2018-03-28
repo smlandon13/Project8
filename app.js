9febcacc79b15a40def24a88261d028c96facf0f
Jared <jaredcol13@gmail.com>
2018-03-28 10:18:18 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
